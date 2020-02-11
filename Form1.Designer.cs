24127cbf18a1389fb89a819b3211d2ed61814b7c
taerd <ffifalafiesta@gmail.com>
2020-02-11 18:39:44 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace IAmBall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(634, 512);
+            this.panel1.TabIndex = 0;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.Resize += new System.EventHandler(this.panel1_Resize);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 536);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IAmBall
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int h=30,w=30,px,py,dx,dy;
+        Random r = new Random();
+        Graphics g;
+        Pen p = new Pen(Color.GreenYellow, 3);
+        Brush b = new SolidBrush(Color.Red);
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            g = panel1.CreateGraphics();
+            px = panel1.Height / 2;
+            py = panel1.Width / 2;
+            dx = r.Next(-5, 5);
+            dy = r.Next(-5, 5);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+            px += dx;
+            py += dy;
+            g.DrawEllipse(p, px, py, w, h);
+            g.FillEllipse(b, px, py, w, h);
+            if(py>=panel1.Height-dy-h || py < Math.Abs(dy))
+            {
+                dy *= -1;
+                if(py >= panel1.Height - dy - h)
+                {
+                    panel1.Refresh();
+                    py = panel1.Height - Math.Abs(dy) - w;
+                    g.DrawEllipse(p, px, py, w, h);
+                    g.FillEllipse(b, px, py, w, h);
+                }
+            }
+            if(px >= panel1.Width - dx - w || px < Math.Abs(dx))
+            {
+                dx *= -1;
+                if(px>=panel1.Width - dx- w)
+                {
+                    panel1.Refresh();
+                    px = panel1.Width - Math.Abs(dx) - h;
+                    g.DrawEllipse(p, px, py, w, h);
+                    g.FillEllipse(b, px, py, w, h);
+                }
+            }
+        }
+
+        private void panel1_Resize(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+        }
+    }
+}
